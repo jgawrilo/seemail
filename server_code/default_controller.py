0f26ab75f3ab830293bca735a7bf32b4ff76c46e
rosteen <richard.g.osteen@gmail.com>
2019-01-02 09:27:26 -0500

    Most functionality coded, need to finish email send and add logging

--- server_code/default_controller.py
+++ server_code/default_controller.py
@@ -7,8 +7,25 @@ from swagger_server.models.error import Error  # noqa: E501
 from swagger_server.models.user import User  # noqa: E501
 from swagger_server import util
 
-users_r = redis.StrictRedis(host='localhost', port=6379, db=1)
-bots_r = redis.StrictRedis(host='localhost', port=6379, db=2)
+import imbox
+import random
+import string
+import json
+import os
+from glob import glob
+from kafka import KafkaProducer
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+# mailinabox management functions
+import sys
+sys.path.append('/root/mailinabox/management/')
+import utils
+import mailconfig
+
+def generate_password(length = 18):
+    return ''.join([random.choice(string.ascii_letters + string.digits ) for n in range(length)])
 
 def create_bot_account_post(user):  # noqa: E501
     """Create a bot email account to send/receive messages from.
@@ -20,15 +37,27 @@ def create_bot_account_post(user):  # noqa: E501
 
     :rtype: bool
     """
-    res_codes ={True: "Success", False: "Failed"}
+    bots_r = redis.StrictRedis(host='localhost', port=6379, db=2)
+    responses = {}
+    res_codes = {True: "Success", False: "Failed"}
     if connexion.request.is_json:
         user = User.from_dict(connexion.request.get_json())  # noqa: E501
 
+    # Load mailinabox env variables
+    env = utils.load_environment()
+
     # Generate a password and create the actual email account
+    pw = generate_password()
+
+    # Add mailbox for bot
+    res = mailconfig.add_mail_user(user, pwd, "", env)
+    reponses['mailbox'] = res
 
     # Add to our Redis bot account db
     res = bots_r.set(user, 1)
-    return res_codes[res]
+    responses['redis'] = res_codes[res]
+    
+    return responses
 
 
 def get_all_users():  # noqa: E501
@@ -39,17 +68,22 @@ def get_all_users():  # noqa: E501
 
     :rtype: List[User]
     """
+    bots_r = redis.StrictRedis(host='localhost', port=6379, db=2)
+    deactivated_bots_r = redis.StrictRedis(host='localhost', port=6379, db=3)
     ## Not sure if this will work on all email implementations. 
     ## Needs location of mailboxes on server, and permission to access that location
     users = []
     domains = os.listdir('/home/user-data/mail/mailboxes')
     for domain in domains:
-        temp_users = os.listdir('/home/user-data/mail/mailboxes/chunkman.com/')
+        temp_users = os.listdir('/home/user-data/mail/mailboxes/{}/'.format(domain))
         for name in temp_users:
             users.append("{}@{}".format(name, domain).encode('utf-8'))
     # Get bots to exclude, we only want to return real users
     bots = []
     for key in bots_r.scan_iter():
+        bots.append(key)
+    # also exclude deactivated bots
+    for key in deactivated_bots_r.scan_iter():
         bots.append(key)
     users = list(set(users) - set(bots))
     return users
@@ -65,6 +99,7 @@ def monitor_users_get(email_addresses):  # noqa: E501
 
     :rtype: List[bool]
     """
+    users_r = redis.StrictRedis(host='localhost', port=6379, db=1)
     res_codes = {True: "Success", False: "Failed"}
     results = []
     for address in email_addresses:
@@ -83,9 +118,12 @@ def remove_bot_account_get(email_addresses):  # noqa: E501
 
     :rtype: bool
     """
-    # Delete account from Redis bot account db
+    bots_r = redis.StrictRedis(host='localhost', port=6379, db=2)
+    deactivated_bots_r = redis.StrictRedis(host='localhost', port=6379, db=3)
+    # Delete account from Redis bot account db and add to defunct bots db
     for address in email_addresses:
         res = bots_r.delete(address)
+        res2 = deactivated_bots_r.set(address, 1)
     # Actually remove the email account? Or leave it for later analysis?
     return res_codes[res]
 
@@ -104,6 +142,18 @@ def request_mail_history_get(email_addresses, request_key, back_to_iso_date_stri
 
     :rtype: List[bool]
     """
+    producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    for address in email_addresses:
+        user = address.split('@')[0]
+        domain = address.split('@')[1]
+        filelist = glob('/home/user-data/mail/mailboxes/{}/{}/*/*'.format(domain, user))
+        for filename in filelist:    
+            mail = "".join(open(filename).readlines())
+            mail_dict = imbox.parser.parse_email(mail)
+            # Need to decide whether to put transform function in this file or other
+            transformed = transform_email(mail_dict)
+            producer.send("history", transformed)
+            producer.flush()
     return 'do some magic!'
 
 
@@ -119,6 +169,11 @@ def request_send_mail_post(email):  # noqa: E501
     """
     if connexion.request.is_json:
         email = Email.from_dict(connexion.request.get_json())  # noqa: E501
+
+    s = smtplib.SMTP("localhost:smtp")
+    # Build MIME email from email object? Need to double check input format
+    msg = MIMEMultipart()
+    s.sendmail(from_address, to_addresses, msg.as_string())
     return 'do some magic!'
 
 
@@ -132,9 +187,10 @@ def unmonitor_users_get(email_addresses):  # noqa: E501
 
     :rtype: List[bool]
     """
+    users_r = redis.StrictRedis(host='localhost', port=6379, db=1)
     res_codes = {1: "Success", 0: "Failed"}
     results = []
     for address in email_addresses:
         res = users_r.delete(address)
-	results.append(res_codes[res])
+        results.append(res_codes[res])
     return results
