06eae9945535b58d6f730e07273e385114e7181b
rosteen <richard.g.osteen@gmail.com>
2019-01-10 10:53:31 -0500

    Got the json structure to print correctly, forgot to load it first

--- server_code/kafka_consumer.py
+++ server_code/kafka_consumer.py
@@ -6,9 +6,9 @@ def main(topic):
     # This will constantly poll for new items coming off the topic
     consumer = kafka.KafkaConsumer(topic)
     for message in consumer:
-        print("{}:{}:{}: key={} value={}".format(message.topic, message.partition,
+        print("{}:{}:{}: key={} value=".format(message.topic, message.partition,
                                           message.offset, message.key))
-        print(json.dumps(message.value.decode('utf-8'), indent = 2))
+        print(json.dumps(json.loads(message.value.decode('utf-8')), indent = 4))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
