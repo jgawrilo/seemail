cf9ed77d36787f385db7f43a3bc5d0fa59aae9b0
rosteen <richard.g.osteen@gmail.com>
2019-01-21 19:47:13 -0500

    Working on parsing the content JSON

--- jpl_replay_daemon.py
+++ jpl_replay_daemon.py
@@ -4,10 +4,41 @@ import json
 import smtplib
 from datetime import datetime, timedelta()
 from time import sleep
+import requests
 
-def main():
-    pass
+def send_email(row, s):
+    email_ids = row[0]
+    filename  = row[2]
+    # Load email json
+    with open(filename, 'r') as f:
+        email_json = json.load(f)
+        content = email_json["body"]["content"]
+        subject = content.split("Subject: ")[-1].split("\n")[0]
+        print("Subject: {}".format(subject))
+        from_str = content.split("From: ")[-1].split("\n")[0]
+        to_str = content.split("To: ")[-1].split("\n")[0]
+        body = ""
 
+    # Get chunkman email addresses that correspond to the JPL email addresses
+
+    attachments = []
+
+    # Parse the email content to build the email object
+    email = {
+         "sent_from": from_address,
+         "sent_to": [],
+         "sent_cc": [],
+         "sent_bcc": [],
+         "body": "",
+         "subject": "",
+         "attachments": attachments,
+         "reply_to_id": "",
+         "forward_id": "",
+         "headers": []
+         }
+    
+    # Send the email via the swagger API
+    res = requests.post("https://box.chunkman.com:8080/requestSendMail", json=email)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,13 +52,20 @@ if __name__ == "__main__":
     conn1 = sql.connect("/home/user-data/mail/jpl_emails.sqlite")
     cur1 = conn1.cursor()
 
+    # Connect to the smtp server
+    s = smtplib.SMTP("localhost:587")
 
     start_dt = datetime.now()
     while true:
         end_dt = start_dt + timedelta(seconds = 60)
         # Get the emails to send this minute and send them
-        stmt = "select * from "
+        stmt = "select * from abuse where replay_timestamp > {} and replay_timestamp <= {}".format(start_dt, end_dt)
+        cur1.execute(stmt)
+        res = cur1.fetchall()
 
+        # Send the emails
+        for row in res:
+            send_email(row)
 
         # Set the start time to the next minute and sleep for the rest of this minute
         # Should probably make sure email sends didn't take longer than expected 
