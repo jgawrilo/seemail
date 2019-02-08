73494ba15155f47ab6674c32979ab4f850f33c66
rosteen <richard.g.osteen@gmail.com>
2019-02-08 15:41:45 -0500

    Adding script for network graph work

---
+++ network_graph.py
@@ -0,0 +1,9 @@
+#
+#  Functions for tracking email interactions via a network graph
+#
+
+import argparse
+import networkx as nx
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
